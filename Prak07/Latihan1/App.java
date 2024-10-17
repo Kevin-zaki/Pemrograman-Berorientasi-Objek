3a92d82314af7a75e5bda5a83b3832a63d8db985
Kevin-zaki <kevinkzmh@gmail.com>
2024-10-17 15:10:14 +0700

    Add files via upload

---
+++ Prak07/Latihan1/App.java
@@ -0,0 +1,9 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Luar l = new Luar();
+        Luar.Dalam d = l.new Dalam();
+
+        d.bicara();
+    }
+}
